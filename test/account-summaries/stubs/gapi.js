fb52a7272e42a12d5e6fbb6c7c5b2746dd752fd7
Philip Walton <philip@philipwalton.com>
2014-10-10 11:52:09 -0700

    Use the promise constructor returned by gapi instead of the polyfill.

--- test/account-summaries/stubs/gapi.js
+++ test/account-summaries/stubs/gapi.js
@@ -1,25 +1,31 @@
-/* global gapi:true */
+/* global gapi:true, Promise:true */
 
 var fixtures = require('../fixtures');
+
+// Polyfill Promises for node.
+Promise = require('native-promise-only');
 
 // Assign this globally because that's how it is IRL.
 gapi = {client: {analytics: {management: {accountSummaries: {}}}}};
 
 gapi.client.analytics.management.accountSummaries.list = function(options) {
 
+  options = options || {};
+
+  var response = { result: fixtures.get() };
+
+  response.result.startIndex = options['start-index'] || 1;
+  response.result.itemsPerPage = options['max-results'] || 1000;
+
+  // If a user has no accounts, items will not be defined.
+  if (response.result.items) {
+    response.result.items = response.result.items.splice(
+        response.result.startIndex - 1, response.result.itemsPerPage);
+  }
+
   return {
-    execute: function(cb) {
-      var results = fixtures.get();
-      results.startIndex = options['start-index'] || 1;
-      results.itemsPerPage = options['max-results'] || 1000;
-
-      // If a user has no accounts, items will not be defined.
-      if (results.items) {
-        results.items = results.items.splice(
-            results.startIndex - 1,results.itemsPerPage);
-      }
-
-      setTimeout(cb.bind(null, results, 0));
+    then: function(fn) {
+      return Promise.resolve(fn(response));
     }
   };
 };
